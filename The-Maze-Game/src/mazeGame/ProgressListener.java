9e125ea3edb6234e83d05e092bf18310ec0b1662
Ranjana <ranjanarajeev0123@gmail.com>
2022-12-20 10:49:11 +0530

    added progress listner class

---
+++ The-Maze-Game/src/mazeGame/ProgressListener.java
@@ -0,0 +1,29 @@
+package mazeGame;
+
+/**
+ * The listener interface for receiving level progress feedback.
+ * The class that is interested in processing level progress feedback has to
+ * implements this interface.
+ * A progress feedback is generated when the player has either completed or failed a level.
+ */
+
+public interface ProgressListener {
+
+    /**
+     * Invoked when level one is completed.
+     * See the class description of {LevelOne} for the level completion details.
+     */
+    void levelOneCompleted();
+
+    /**
+     * Invoked when level two is completed.
+     * See the class description of {LevelTwo} for the level completion details.
+     */
+    void levelTwoCompleted();
+
+    /**
+     * Invoked when the Player is dead to end the game.
+     */
+    void levelFailed();
+}
+
